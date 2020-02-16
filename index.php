d73b096a4ccfd56c6f88af8ca014a2a8dc0fb9ad
faklarion <faisal.hazard17@gmail.com>
2020-02-16 10:55:32 +0800

    testtt

--- index.php
+++ index.php
@@ -53,7 +53,14 @@ $app->post('/', function ($request, $response)
 			$result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
 			return $result->getHTTPStatus() . ' ' . $result->getRawBody();
 		
-		}
+		} elseif(strtolower($userMessage) == 'hai')
+		{
+			$message = "hai juga";
+            $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message);
+			$result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
+			return $result->getHTTPStatus() . ' ' . $result->getRawBody();
+		
+		} 
 	}
 	
 
